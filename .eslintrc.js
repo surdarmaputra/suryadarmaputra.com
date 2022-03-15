366d85104675766f7e3e8c14dc105fc3ea223196
Surya Darma Putra <surya.igede@gmail.com>
2022-03-16 03:34:49 +0800

    chore(linter): add delimiter and empty line linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,29 @@ module.exports = {
   extends: ["@remix-run/eslint-config"],
   rules: {
     '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+      },
+    ],
+    '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/semi': ['error'],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+        maxBOF: 0,
+        maxEOF: 0,
+      },
+    ],
     'react/jsx-sort-props': 'error',
   },
 };
