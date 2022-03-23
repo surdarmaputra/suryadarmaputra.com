12d6648d5cc4da417dc8b030c51b5d27a3533970
Surya Darma Putra <surya.igede@gmail.com>
2022-03-23 21:50:43 +0800

    feat: update BrandHero

--- app/components/sections/BrandHero.tsx
+++ app/components/sections/BrandHero.tsx
@@ -1,12 +1,12 @@
 export default function BrandHero() {
   return (
     <section className="text-center mt-20 mb-28 md:mb-32">
-      <h1 className="text-6xl font-extrabold tracking-tighter mb-3 text-black dark:text-slate-100">
-        Surya Darma Putra
+      <h1 className="text-5xl font-bold tracking-tighter mb-3 text-black dark:text-slate-100 leading-tight">
+        <span className="block md:inline">👋 Hello,</span>{' '}
+        <span>I'm Surya</span>
       </h1>
       <p className="px-4 font-light text-slate-500">
-        Software engineer. Writing about software engineering and everything in
-        between.
+        I do web development. Thanks for coming!
       </p>
     </section>
   );
