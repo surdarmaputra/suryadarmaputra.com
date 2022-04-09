619c75b3d483bac89e06c9314ca95f1573a91f45
Surya Darma Putra <surya.igede@gmail.com>
2022-04-10 01:06:34 +0800

    chore: enable post query filtesr

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -4,11 +4,11 @@ import { getPagesFromDatabase } from '../app/libs/notion';
 
 export function fetchPosts(): Promise<GetPageResponse[]> {
   return getPagesFromDatabase(process.env.NOTION_DATABASE_ID, {
-    // filter: {
-    //   property: 'publish',
-    //   checkbox: {
-    //     equals: true,
-    //   },
-    // },
+    filter: {
+      property: 'publish',
+      checkbox: {
+        equals: true,
+      },
+    },
   });
 }
