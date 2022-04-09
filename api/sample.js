ca28b5037e2b81e1597e26d0762a13f16f56abfc
Surya Darma Putra <surya.igede@gmail.com>
2022-04-09 16:15:48 +0800

    chore: test fs

--- api/sample.js
+++ api/sample.js
@@ -1,5 +1,17 @@
 // API placeholder
+import fs from 'fs/promises';
+import path from 'path';
 
-export default function (request, response) {
-  response.status(200).send('Hi!');
+async function getDir() {
+  const dirNameContent = await fs.readdir(path.join(__dirname));
+
+  return {
+    dirName: __dirname,
+    dirNameContent: JSON.stringify(dirNameContent),
+  };
+}
+
+export default async function (request, response) {
+  const dir = getDir();
+  response.status(200).json(dir);
 }
