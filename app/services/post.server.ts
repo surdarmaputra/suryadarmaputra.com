3e0879e8e05015ed14dfc5d56d546aa4c3632e05
Surya Darma Putra <surya.igede@gmail.com>
2022-04-09 14:40:02 +0800

    chore: add prepare:api script

--- app/services/post.server.ts
+++ app/services/post.server.ts
@@ -35,6 +35,7 @@ function formatPost(
   const updatedAt = new Date(properties.updated_at);
   const slug = fileName.replace(/\.json$/, '');
   const href = `/blog/${slug}`;
+
   return {
     category: properties.category,
     date,
