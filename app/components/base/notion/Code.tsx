450d83f5a79aef982a0feb7466909a38ace1c529
Surya Darma Putra <surya.igede@gmail.com>
2022-09-17 17:30:15 +0800

    feat: expandable code snippet

--- app/components/base/notion/Code.tsx
+++ app/components/base/notion/Code.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { coldarkDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
@@ -13,19 +14,50 @@ interface CodeProps extends BlockComponentProps {
 }
 
 const shellLanguages = ['shell', 'bash'];
+const expandableHeightThreshold = 400;
 
 export function Code({ block }: CodeProps) {
+  const codeRef = useRef<HTMLDivElement>(null);
+  const [expanded, setExpanded] = useState(true);
+
   const { language, rich_text: richTexts } = block.code;
   const codeText = concatPlainTexts(richTexts as RichTextBlock[]);
 
+  useEffect(() => {
+    if (
+      codeRef.current?.offsetHeight &&
+      codeRef.current.offsetHeight > expandableHeightThreshold
+    ) {
+      setExpanded(false);
+    }
+  }, [block]);
+
   return (
-    <SyntaxHighlighter
-      customStyle={{ marginBottom: 16 }}
-      language={language}
-      showLineNumbers={!shellLanguages.includes(language)}
-      style={coldarkDark}
+    <div
+      className={`relative mb-4 overflow-y-hidden transition ${
+        expanded ? 'h-fit' : 'h-96'
+      }`}
+      ref={codeRef}
     >
-      {codeText}
-    </SyntaxHighlighter>
+      {!expanded && (
+        <>
+          <div className="absolute bottom-0 h-32 w-full bg-gradient-to-b from-transparent to-white dark:to-slate-900"></div>
+          <button
+            className="absolute bottom-5 left-1/2 -translate-x-1/2 rounded bg-slate-800 px-4 py-1 text-sm text-slate-300  shadow-lg transition hover:bg-slate-700"
+            onClick={() => setExpanded(true)}
+          >
+            Expand
+          </button>
+        </>
+      )}
+      <SyntaxHighlighter
+        // customStyle={{ marginBottom: 16 }}
+        language={language}
+        showLineNumbers={!shellLanguages.includes(language)}
+        style={coldarkDark}
+      >
+        {codeText}
+      </SyntaxHighlighter>
+    </div>
   );
 }
