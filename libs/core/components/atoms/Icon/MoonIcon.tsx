6f3349f3ee83dd6f69776e6373fc1a43240f7709
surdarmaputra <surya.igede@gmail.com>
2023-07-23 19:09:53 +0700

    add icon

---
+++ libs/core/components/atoms/Icon/MoonIcon.tsx
@@ -0,0 +1,22 @@
+import { HTMLAttributes, ReactElement } from 'react';
+
+export default function MoonIcon({
+  className,
+}: HTMLAttributes<SVGAElement>): ReactElement {
+  return (
+    <svg
+    className={`h-5 w-5" ${className}`}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ libs/core/components/atoms/Icon/SunIcon.tsx
@@ -0,0 +1,22 @@
+import { HTMLAttributes, ReactElement } from 'react';
+
+export default function SunIcon({
+  className,
+}: HTMLAttributes<SVGAElement>): ReactElement {
+  return (
+    <svg
+    className={`h-5 w-5" ${className}`}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
