90bb205dabe54a591c90e99237da875610143b7d
surdarmaputra <surya.igede@gmail.com>
2024-11-24 03:19:38 +0700

    refactor: landing page into module

--- app/libs/notion/types.ts
+++ app/libs/notion/types.ts
@@ -53,25 +53,25 @@ export type RichTextBlock = {
     underline: boolean;
     code: boolean;
     color:
-    | 'default'
-    | 'gray'
-    | 'brown'
-    | 'orange'
-    | 'yellow'
-    | 'green'
-    | 'blue'
-    | 'purple'
-    | 'pink'
-    | 'red'
-    | 'gray_background'
-    | 'brown_background'
-    | 'orange_background'
-    | 'yellow_background'
-    | 'green_background'
-    | 'blue_background'
-    | 'purple_background'
-    | 'pink_background'
-    | 'red_background';
+      | 'default'
+      | 'gray'
+      | 'brown'
+      | 'orange'
+      | 'yellow'
+      | 'green'
+      | 'blue'
+      | 'purple'
+      | 'pink'
+      | 'red'
+      | 'gray_background'
+      | 'brown_background'
+      | 'orange_background'
+      | 'yellow_background'
+      | 'green_background'
+      | 'blue_background'
+      | 'purple_background'
+      | 'pink_background'
+      | 'red_background';
   };
   plain_text: string;
   href: string | null;
