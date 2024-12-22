a6282044650cda32d2d64c42e66d1e84626c014c
I Gede Putu Surya Darma Putra <gede.putra@gojek.com>
2024-12-22 19:02:58 +0700

    chore: separate default build with netlify build and fix netlify plugin order

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const plugins = [
+  reactRouter(),
+  tsconfigPaths(),
+];
+
+if (process.env.NETLIFY_DEPLOYMENT === 'true') {
+  plugins.push(netlifyPlugin());
+}
+
 export default defineConfig({
-  plugins: [
-    netlifyPlugin(),
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+  plugins,
   ssr: {
     optimizeDeps: {
       include: ['lodash'],
