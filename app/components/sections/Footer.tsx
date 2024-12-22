7911cc0e314ecd66ec873356152248bf3c816f98
I Gede Putu Surya Darma Putra <gede.putra@gojek.com>
2024-12-23 01:05:27 +0700

    chore: update color on footer

--- app/components/sections/Footer.tsx
+++ app/components/sections/Footer.tsx
@@ -9,12 +9,12 @@ export const links = [
 export function Footer() {
   return (
     <footer className="mt-20 py-8 text-center md:mt-36">
-      <span className="mb-0.5 block text-sm font-light text-slate-600 dark:text-slate-500">
+      <span className="mb-0.5 block text-sm font-light text-slate-600 dark:text-slate-400">
         Surya Darma Putra
       </span>
       {links.map(({ label, href }) => (
         <SmartLink
-          className="animated-link mr-3 pb-1 text-xs font-light text-slate-500 last:mr-0"
+          className="animated-link mr-3 pb-1 text-xs font-extralight text-slate-500 dark:text-slate-400 last:mr-0"
           href={href}
           key={href}
         >
