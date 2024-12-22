dc42049a3f0838d20c9d27494db822fa3ab0553a
I Gede Putu Surya Darma Putra <gede.putra@gojek.com>
2024-12-22 18:13:05 +0700

    chore: add netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+import netlifyPlugin from '@netlify/vite-plugin-react-router';
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    netlifyPlugin(),
     reactRouter(),
     tsconfigPaths(),
   ],
