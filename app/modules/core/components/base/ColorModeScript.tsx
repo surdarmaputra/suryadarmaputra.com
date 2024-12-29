f4354fe8f1ef011be53fb57eaa7449f11aebbe01
Surya Darma Putra <surya.igede@gmail.com>
2024-12-29 17:09:39 +0800

    fix tap target and browser error issue

--- app/modules/core/components/base/ColorModeScript.tsx
+++ app/modules/core/components/base/ColorModeScript.tsx
@@ -1,19 +1,19 @@
-import InlineScript from './InlineScript';
-
 export default function ColorModeScript() {
   return (
-    <InlineScript
-      body={`
-        if (localStorage.theme === 'light' || !('theme' in localStorage)) {
-          document.documentElement.classList.add('light');
-          document.documentElement.classList.remove('dark');
-        }
-          
-        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-          document.documentElement.classList.add('dark');
-          document.documentElement.classList.remove('light')
-        }  
-      `}
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          if (localStorage.theme === 'light' || !('theme' in localStorage)) {
+            document.documentElement.classList.add('light');
+            document.documentElement.classList.remove('dark');
+          }
+            
+          if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+            document.documentElement.classList.remove('light')
+          }  
+        `,
+      }}
     />
   );
 }
