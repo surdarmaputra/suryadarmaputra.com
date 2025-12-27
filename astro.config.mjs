053335f4665adfdbb65ba13cfa6e0116f8099cce
surdarmaputra <surya.igede@gmail.com>
2025-12-28 04:27:50 +0800

    feat: prefetch link by default and implement client router

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  prefetch: {
+    defaultStrategy: "viewport",
+    prefetchAll: true,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
