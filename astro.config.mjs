a44ab3ebdafa8a730797ea8858769e5c2f567bfb
surdarmaputra <surya.igede@gmail.com>
2025-12-31 14:51:56 +0800

    feat: icon using iconify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 export default defineConfig({
   prefetch: {
@@ -11,4 +12,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [icon()],
 });
